47d8a348f5a2ff73f328bc5e30096626f21c0070
Ruhshan <ruhshan.ahmed@gmail.com>
2017-10-22 12:38:21 +0600

    updated fileupload admin

--- cardinfo/admin.py
+++ cardinfo/admin.py
@@ -6,5 +6,9 @@ class CreditCardAdmin(admin.ModelAdmin):
     list_display = ('national_id', 'name_on_card', 'card_number', 'expire_on', 'card_cif','card_cvv','bank_cif')
     search_fields = ('national_id', 'name_on_card')
 
+class FileUploadAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'csv_file', 'uploaded_at')
+    #search_fields = ('national_id', 'name_on_card')
+
 admin.site.register(CreditCard, CreditCardAdmin)
-admin.site.register(FileUpload)
+admin.site.register(FileUpload, FileUploadAdmin)
