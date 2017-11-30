3d41be602bf7f3ff296c84ebe9822d92a659e56c
Ruhshan <ruhshan.ahmed@gmail.com>
2017-11-30 18:02:07 +0600

    circle flavoured login page

--- bankinfo/urls.py
+++ bankinfo/urls.py
@@ -1,7 +1,11 @@
 from bankinfo.api import *
+from django.conf.urls import *
 from rest_framework import routers
 router = routers.DefaultRouter(trailing_slash=True)
-
+from . import views
 router.register(r'api', BankRetrive)
 
+#urlpatterns = [url(r"^login2/", views.loginView)]
+
 urlpatterns = router.urls
+
--- bankinfo/views.py
+++ bankinfo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def loginView(request):
+    return render(request, "admin/circle_login.html")
